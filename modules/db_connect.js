673e49ec112c440426e5f19da4ef2b84a300f396
Andy Lin <ld130101@gmail.com>
2022-11-13 13:20:44 +0800

    資料庫連接

--- modules/db_connect.js
+++ modules/db_connect.js
@@ -1,13 +1,13 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'pet_test2',
-    waitForConnections:true,
-    connectionLimit:5,
-    queueLimit:0
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'pet_product',
+    waitForConnections: true,
+    connectionLimit: 5,
+    queueLimit: 0
 });
 
 module.exports = pool.promise();
