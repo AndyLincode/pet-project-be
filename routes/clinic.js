8dc974efd8ec1dbfd3081250fe720c2c12239d97
Kunda <love60066@gmail.com>
2022-11-25 13:54:35 +0800

    test_opay

--- routes/clinic.js
+++ routes/clinic.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const db = require(__dirname + '/../modules/db_connect');
 const jwt = require('jsonwebtoken');
 const moment = require('moment-timezone'); // 日期格式(選擇性)
+const upload = require(__dirname + '/../modules/upload_img');
+const opay = require('opay_payment_nodejs');
+const ShortUniqueId = require('short-unique-id');
+const dayjs = require('dayjs');
 
 router.use((req, res, next) => {
   next();
@@ -71,7 +75,7 @@ async function getMemberData(req, res) {
   return { rows };
 }
 
-async function getPetData(req,res) {
+async function getPetData(req, res) {
   let sid = req.params.sid ? req.params.sid.trim() : '';
 
   let where = `WHERE pd.member_sid = ${sid}`;
@@ -84,9 +88,83 @@ async function getPetData(req,res) {
 
   return { rows };
 }
+
+//歐付寶金流
+//按下結帳API
+router.get('/paymentaction', (req, res) => {
+  const uid = new ShortUniqueId({ length: 20 });
+  const daytime = dayjs(new Date()).format('YYYY/MM/DD HH:mm:ss');
+  let base_param = {
+    MerchantTradeNo: uid(), //請帶20碼uid, ex: f0a0d7e9fae1bb72bc93
+    MerchantTradeDate: daytime, //ex: 2017/02/13 15:45:30
+    TotalAmount: '100',
+    TradeDesc: '企鵝玩偶 一隻',
+    ItemName: '企鵝玩偶 300元 X 1#企鵝玩偶 200元 X 1',
+    ReturnURL: 'http://localhost:3000/', // 付款結果通知URL  https://developers.opay.tw/AioMock/MerchantReturnUrl
+    OrderResultURL: '', // 在使用者在付款結束後，將使用者的瀏覽器畫面導向該URL所指定的URL
+    EncryptType: 1,
+    // ItemURL: 'http://item.test.tw',aw
+    Remark: '該服務繳費成立時，恕不接受退款。',
+    // HoldTradeAMT: '1',
+    // StoreID: '',
+    // UseRedeem: ''
+  };
+  console.log(uid());
+  let create = new opay();
+  // let parameters = {};
+  // let invoice = {};
+  try {
+    let htm = create.payment_client.aio_check_out_credit_onetime(
+      (parameters = base_param)
+    );
+    // console.log(htm);
+    res.json(htm);
+  } catch (err) {
+    // console.log(err);
+    let error = {
+      status: '500',
+      stack: '',
+    };
+    res.json(error);
+  }
+});
+
+router.post('/payment', (req, res) => {
+  var rtnCode = req.body.RtnCode;
+  var simulatePaid = req.body.SimulatePaid;
+  var merchantID = req.body.MerchantID;
+  var merchantTradeNo = req.body.MerchantTradeNo;
+  var storeID = req.body.StoreID;
+  var rtnMsg = req.body.RtnMsg;
+  // var tradeNo = req.body.TradeNo;
+  var tradeAmt = req.body.TradeAmt;
+  // var payAmt = req.body.PayAmt;
+  var paymentDate = req.body.PaymentDate;
+  var paymentType = req.body.PaymentType;
+  // var paymentTypeChargeFee = req.body.PaymentTypeChargeFee;
+
+  let paymentInfo = {
+    merchantID: merchantID,
+    merchantTradeNo: merchantTradeNo,
+    storeID: storeID,
+    rtnMsg: rtnMsg,
+    paymentDate: paymentDate,
+    paymentType: paymentType,
+    tradeAmt: tradeAmt
+  }
+
+  //(添加simulatePaid模擬付款的判斷 1為模擬付款 0 為正式付款)
+  //測試環境
+  if (rtnCode === "1" && simulatePaid === "1") {
+    // 這部分可與資料庫做互動
+    res.write("1|OK");
+    res.end();
+  }
+})
+
 //新增資料
 
-router.post('/add', async (req, res) => {
+router.post('/add', upload.none(), async (req, res) => {
   const output = {
     success: false,
     code: 0,
@@ -95,11 +173,12 @@ router.post('/add', async (req, res) => {
   };
 
   const sql =
-    'INSERT INTO `reserve_data`(`member_sid`,`pet_sid`,`symptom`,`date`,`time`) VALUES (?,?,?,?,?)';
+    'INSERT INTO `reserve_data`(`member_sid`,`pet_sid`,`clinic_sid`,`symptom`,`date`,`time`) VALUES (?,?,?,?,?,?)';
 
   const [result] = await db.query(sql, [
-    req.body.membersid,
-    req.body.petsid,
+    req.body.member_sid,
+    req.body.pet_sid,
+    req.body.clinic_sid,
     req.body.symptom,
     req.body.date,
     req.body.time,
@@ -107,7 +186,9 @@ router.post('/add', async (req, res) => {
 
   if (result.affectedRows) output.success = true;
 
-  req.json(output);
+  // console.log(result);
+
+  res.json(output);
 });
 
 router.get('/list', async (req, res) => {
@@ -131,7 +212,7 @@ router.get('/member/:sid', async (req, res) => {
 });
 
 router.get('/pet/:sid', async (req, res) => {
-  res.json(await getPetData(req,res));
+  res.json(await getPetData(req, res));
 });
 
 router.post('/login-api', async (req, res) => {
